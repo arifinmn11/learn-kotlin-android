b0630e099e65aed5bc8c336e480806de83517173
arifinmn <nurarifinmuhammad@gmail.com>
2021-02-18 21:02:53 +0700

    first commit master

---
+++ app/src/main/java/com/example/latihanframgent/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.latihanframgent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.latihanframgent.interfaces.Communicator
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/latihanframgent/interfaces/Communicator.kt
@@ -0,0 +1,5 @@
+package com.example.latihanframgent.interfaces
+
+public interface Communicator {
+    fun passDataCom(editTextInput: String)
+}
